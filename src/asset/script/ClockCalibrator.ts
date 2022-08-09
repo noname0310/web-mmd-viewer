1b4e64b28a809340a6d48ff9e62dac6136eaf4a0
noname <48761044+noname0310@users.noreply.github.com>
2022-08-09 22:12:01 +0900

    rollback

--- src/asset/script/ClockCalibrator.ts
+++ src/asset/script/ClockCalibrator.ts
@@ -61,7 +61,7 @@ export class ClockCalibrator implements IAnimationClock {
             const performanceNow = performance.now() / 1000;
             const performanceDelta = (performanceNow - this._lastNow) * this.playbackRate;
 
-            if (0.5 < performanceDelta) {
+            if (1 < performanceDelta) {
                 return this._lastCurrentTime;
             } else {
                 return this._lastCurrentTime + performanceDelta;
