0a4e9b4259f20f33782e4d609c97b62014145b1e
noname <48761044+noname0310@users.noreply.github.com>
2022-08-16 05:19:56 +0900

    add grant solver override

--- src/asset/script/MMDAnimationHelperOverride.ts
+++ src/asset/script/MMDAnimationHelperOverride.ts
@@ -1,4 +1,5 @@
-import { MMDAnimationHelper } from "three/examples/jsm/animation/MMDAnimationHelper";
+import { GrantSolver as ThreeGrantSolver, MMDAnimationHelper } from "three/examples/jsm/animation/MMDAnimationHelper";
+import * as THREE from "three/src/Three";
 
 import { MMdPhysicsOverride } from "./MMDPhysicsOverride";
 
@@ -16,5 +17,77 @@ export class MMDAnimationHelperOverride extends MMDAnimationHelper {
             mesh.geometry.userData.MMD.constraints,
             params
         );
+    }
+
+    public createGrantSolver(mesh: THREE.SkinnedMesh): ThreeGrantSolver {
+        return new GrantSolver(mesh, mesh.geometry.userData.MMD.grants) as unknown as ThreeGrantSolver;
+    }
+}
+
+type Grant = {
+    index: number;
+    parentIndex: number;
+    isLocal: boolean;
+    affectPosition: boolean;
+    affectRotation: boolean;
+    ratio: number;
+};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const _q = new THREE.Quaternion();
+
+class GrantSolver {
+    public mesh: THREE.SkinnedMesh;
+    public grants: Grant[];
+
+    public constructor(mesh: THREE.SkinnedMesh, grants: Grant[] = []) {
+        this.mesh = mesh;
+        this.grants = grants;
+    }
+
+    public update(): GrantSolver {
+        const grants = this.grants;
+
+        for (let i = 0, il = grants.length; i < il; ++i) this.updateOne(grants[i]);
+
+        return this;
+    }
+
+    public updateOne(grant: Grant): GrantSolver {
+        const bones = this.mesh.skeleton.bones;
+        const bone = bones[grant.index];
+        const parentBone = bones[grant.parentIndex];
+
+        if (grant.isLocal) {
+            // TODO: implement
+            if (grant.affectPosition) {
+                //...
+            }
+
+            // TODO: implement
+            if (grant.affectRotation) {
+                //...
+            }
+
+        } else {
+            // TODO: implement
+            if (grant.affectPosition) {
+                //...
+            }
+
+            if (grant.affectRotation) {
+                this.addGrantRotation(bone, parentBone.quaternion, grant.ratio);
+            }
+        }
+
+        return this;
+    }
+
+    public addGrantRotation(bone: THREE.Bone, q: THREE.Quaternion, ratio: number): GrantSolver {
+        _q.set(0, 0, 0, 1);
+        _q.slerp(q, ratio);
+        bone.quaternion.multiply(_q);
+
+        return this;
     }
 }
