81b4e376938117e73baae4bdd3a92f2ac12f6a4f
noname <48761044+noname0310@users.noreply.github.com>
2022-08-25 12:31:10 +0900

    add sampler

--- src/asset/InterpolationTestBootstrapper.ts
+++ src/asset/InterpolationTestBootstrapper.ts
@@ -1,6 +1,7 @@
-import { Bootstrapper, Camera, SceneBuilder } from "the-world-engine";
+import { Bootstrapper, Camera, EditorCameraController, EditorGridRenderer, SceneBuilder } from "the-world-engine";
 
-import { CssImageTrace } from "./script/render/CssImageTrace";
+import { BezierCurve } from "./script/mmd/interpolation/BezierInterpolator";
+import { InterpolateSampler } from "./script/mmd/interpolation/InterpolateSampler";
 
 export class InterpolationTestBootstrapper extends Bootstrapper {
     public run(): SceneBuilder {
@@ -8,10 +9,30 @@ export class InterpolationTestBootstrapper extends Bootstrapper {
 
         return this.sceneBuilder
             .withChild(instantiater.buildGameObject("camera")
-                .withComponent(Camera))
+                .withComponent(Camera, c => {
+                    c.viewSize = 10;
+                })
+                .withComponent(EditorGridRenderer, c => {
+                    c.gridCellWidth = 2;
+                    c.gridCellHeight = 2;
+                    c.lineWidth = 0.1;
+                    c.renderHeight = 30;
+                    c.renderWidth = 30;
+                })
+                .withComponent(EditorCameraController, c => {
+                    c.mouseMoveButton = 0;
+                }))
 
             .withChild(instantiater.buildGameObject("babylon-interpolation-test")
-                .withComponent(CssImageTrace))
+                .withComponent(InterpolateSampler, c => {
+                    c.interpolator = (t: number): number => {
+                        const x1 = 1;
+                        const y1 = 1;
+                        const x2 = 2;
+                        const y2 = 2;
+                        return BezierCurve.interpolate(t, x1, y1, x2, y2);
+                    };
+                }))
         ;
     }
 }
