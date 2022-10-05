7a334cd3afadb7db9c60d1b5a20483cff350d4b3
noname <48761044+noname0310@users.noreply.github.com>
2022-10-05 15:11:10 +0900

    add listViewMode option

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import Ammo from "ammojs-typed";
 import { Game } from "the-world-engine";
-
 import { MmdEditorBootstrapper } from "./asset/MmdEditorBootstrapper";
+
 import { AudioPermissionSolver } from "./asset/script/AudioPermissionSolver";
+import { ListMmdViewBuilder } from "./asset/script/ListMmdViewBuilder";
+
+const listViewMode = false;
 
 async function startGame(): Promise<void> {
     await AudioPermissionSolver.invoke();
     await Ammo(Ammo);
-    const game = new Game(document.getElementById("game_view")!);
-    game.run(MmdEditorBootstrapper);
-    game.inputHandler.startHandleEvents();
+
+    if (listViewMode) {
+        ListMmdViewBuilder.invoke();
+    } else {
+        const game = new Game(document.getElementById("game_view")!);
+        game.run(MmdEditorBootstrapper);
+        game.inputHandler.startHandleEvents();
+    }
 }
 
 startGame();
