06f8c55d40786fe96c51e4ad1dfdc19d4d74b2d4
noname <48761044+noname0310@users.noreply.github.com>
2023-05-17 13:37:47 +0900

    Update PizzicatoDropsBootstrapper.ts

--- src/asset/mmd_scene/PizzicatoDropsBootstrapper.ts
+++ src/asset/mmd_scene/PizzicatoDropsBootstrapper.ts
@@ -31,7 +31,7 @@ export class PizzicatoDropsBootstrapper extends BaseBootstrapper {
             renderer.setPixelRatio(window.devicePixelRatio);
             renderer.shadowMap.enabled = true;
             renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-            renderer.outputColorSpace = THREE.NoColorSpace;
+            renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
             // renderer.toneMapping = THREE.ACESFilmicToneMapping;
             // renderer.toneMappingExposure = 0.5;
             return [new OutlineEffect(renderer), renderer.domElement] as const;
